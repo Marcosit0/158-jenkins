0043d125b3f815b36769f2ef25018340bd8bf446
Romero <marcosrb2001@gmail.com>
2025-05-12 09:49:29 +0200

    test 4385728413

--- flask-app/app-romero.py
+++ flask-app/app-romero.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "lets go ça fonctione pas"
+	return "lets go ça fonctione pas ou peut etre que si"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
